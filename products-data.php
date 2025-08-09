038fc904af13050a8baa3fbe4f13f721b8023702
Nidhi Jain <nidhijain5f2005@gmail.com>
2025-08-09 11:51:08 +0530

    products single

--- products-data.php
+++ products-data.php
@@ -3,35 +3,35 @@ $products = [
     [
         'title' => 'Flat Bars',
         'image' => 'images/panchratnaContent/Products/flatbar.jpg',
-        'url' => 'project-single.html',
+        'url' => 'project-single.php',
         'tag' => 'Flat Bars',
         'categories' => 'Bars'
     ],
     [
         'title' => 'SS Channels',
         'image' => 'images/panchratnaContent/Products/ss-channel.jpg',
-        'url' => 'project-single.html',
+        'url' => 'project-single.php',
         'tag' => 'SS Channel',
         'categories' => 'Channels'
     ],
     [
         'title' => 'SS Equal Angles',
         'image' => 'images/panchratnaContent/Products/ss-equal-angles.jpg',
-        'url' => 'project-single.html',
+        'url' => 'project-single.php',
         'tag' => 'SS Equal Angles',
         'categories' => 'Angles'
     ],
      [
         'title' => 'SS Round Bars',
         'image' => 'images/panchratnaContent/Products/ss-round-bar.jpg',
-        'url' => 'project-single.html',
+        'url' => 'project-single.php',
         'tag' => 'SS Round Bars',
         'categories' => 'Angles'
     ],
      [
         'title' => 'SS Bright Bars',
         'image' => 'images/panchratnaContent/Products/ss-bright-bar.jpg',
-        'url' => 'project-single.html',
+        'url' => 'project-single.php',
         'tag' => 'SS Bright Bars',
         'categories' => 'Angles'
     ],
