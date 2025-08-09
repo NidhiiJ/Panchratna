e4214e469329eba7b81f01244508ae61baee4908
Nidhi Jain <nidhijain5f2005@gmail.com>
2025-08-09 14:18:36 +0530

    industries we serve column

--- industries-we-serve.php
+++ industries-we-serve.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="zxx">
+
 <head>
     <?php $page_title = "Industries We Serve - Panchratna Overseas";
     $page_description = "Learn about Panchratna Overseas";
     include 'headConfig.php';
     ?>
 </head>
+
 <body>
 
     <!-- Preloader Start -->
@@ -17,219 +19,141 @@
     <!-- Header End -->
 
     <!-- Page Header Start -->
-	<div class="page-header page-header-services parallaxie">
-		<div class="container">
-			<div class="row align-items-center">
-				<div class="col-lg-12">
-					<!-- Page Header Box Start -->
-					<div class="page-header-box">
-						<h1 class="text-anime-style-2" data-cursor="-opaque">Industries <span>We Serve</span></h1>
-						<nav class="wow fadeInUp">
+    <div class="page-header page-header-services parallaxie">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-12">
+                    <!-- Page Header Box Start -->
+                    <div class="page-header-box">
+                        <h1 class="text-anime-style-2" data-cursor="-opaque">Industries <span>We Serve</span></h1>
+                        <nav class="wow fadeInUp">
                             <ol class="breadcrumb">
-								<li class="breadcrumb-item"><a href="./">home</a></li>
-								<li class="breadcrumb-item active" aria-current="page">Industries We Serve</li>
-							</ol>
-						</nav>
-					</div>
-					<!-- Page Header Box End -->
-				</div>
-			</div>
-		</div>
-	</div>
-	<!-- Page Header End -->
+                                <li class="breadcrumb-item"><a href="./">home</a></li>
+                                <li class="breadcrumb-item active" aria-current="page">Industries We Serve</li>
+                            </ol>
+                        </nav>
+                    </div>
+                    <!-- Page Header Box End -->
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Page Header End -->
 
     <!-- Page Services Start -->
     <div class="page-services">
         <div class="container">
             <div class="row">
-                <div class="col-lg-3 col-md-6">
-                    <!-- Service Item Start -->
+                <div class="col-md-4 col-12">
                     <div class="service-item">
                         <div class="icon-box">
                             <img src="images/icon-service-1.svg" alt="">
                         </div>
-
                         <div class="service-body">
                             <h3>AUTOMATIVE</h3>
                         </div>
-
-                        <!-- <div class="service-footer">
-                            <a href="service-single.html" class="service-btn">
-                                <img src="images/arrow-dark.svg" alt="">
-                            </a>
-                        </div> -->
                     </div>
-                    <!-- Service Item End -->
                 </div>
 
-                <div class="col-lg-3 col-md-6">
-                    <!-- Service Item Start -->
+                <div class="col-md-4 col-12">
                     <div class="service-item">
                         <div class="icon-box">
                             <img src="images/icon-service-2.svg" alt="">
                         </div>
-
                         <div class="service-body">
                             <h3>ENERGY</h3>
                         </div>
-
-                        <!-- <div class="service-footer">
-                            <a href="service-single.html" class="service-btn">
-                                <img src="images/arrow-dark.svg" alt="">
-                            </a>
-                        </div> -->
                     </div>
-                    <!-- Service Item End -->
                 </div>
 
-                <div class="col-lg-3 col-md-6">
-                    <!-- Service Item Start -->
+                <div class="col-md-4 col-12">
                     <div class="service-item">
                         <div class="icon-box">
                             <img src="images/icon-service-3.svg" alt="">
                         </div>
-
                         <div class="service-body">
                             <h3>WIND MILL</h3>
                         </div>
-
-                        <!-- <div class="service-footer">
-                            <a href="service-single.html" class="service-btn">
-                                <img src="images/arrow-dark.svg" alt="">
-                            </a>
-                        </div> -->
                     </div>
-                    <!-- Service Item End -->
                 </div>
 
-                <div class="col-lg-3 col-md-6">
-                    <!-- Service Item Start -->
+                <div class="col-md-4 col-12">
                     <div class="service-item">
                         <div class="icon-box">
                             <img src="images/icon-service-4.svg" alt="">
                         </div>
-
                         <div class="service-body">
                             <h3>WATER FILTERATION PLANT</h3>
                         </div>
-
-                        <!-- <div class="service-footer">
-                            <a href="service-single.html" class="service-btn">
-                                <img src="images/arrow-dark.svg" alt="">
-                            </a>
-                        </div> -->
                     </div>
-                    <!-- Service Item End -->
                 </div>
 
-                <div class="col-lg-3 col-md-6">
-                    <!-- Service Item Start -->
+                <div class="col-md-4 col-12">
                     <div class="service-item">
                         <div class="icon-box">
                             <img src="images/icon-service-5.svg" alt="">
                         </div>
-
                         <div class="service-body">
                             <h3>PUMPS</h3>
                         </div>
-
-                        <!-- <div class="service-footer">
-                            <a href="service-single.html" class="service-btn">
-                                <img src="images/arrow-dark.svg" alt="">
-                            </a>
-                        </div> -->
                     </div>
-                    <!-- Service Item End -->
                 </div>
 
-                <div class="col-lg-3 col-md-6">
-                    <!-- Service Item Start -->
+                <div class="col-md-4 col-12">
                     <div class="service-item">
                         <div class="icon-box">
                             <img src="images/icon-service-6.svg" alt="">
                         </div>
-
                         <div class="service-body">
                             <h3>BOATS & SHIPS</h3>
                         </div>
-
-                        <!-- <div class="service-footer">
-                            <a href="service-single.html" class="service-btn">
-                                <img src="images/arrow-dark.svg" alt="">
-                            </a>
-                        </div> -->
                     </div>
-                    <!-- Service Item End -->
                 </div>
 
-                <div class="col-lg-3 col-md-6">
-                    <!-- Service Item Start -->
+                <div class="col-md-4 col-12">
                     <div class="service-item">
                         <div class="icon-box">
                             <img src="images/icon-service-7.svg" alt="">
                         </div>
-
                         <div class="service-body">
                             <h3>OIL & GAS</h3>
                         </div>
-
-                        <!-- <div class="service-footer">
-                            <a href="service-single.html" class="service-btn">
-                                <img src="images/arrow-dark.svg" alt="">
-                            </a>
-                        </div> -->
                     </div>
-                    <!-- Service Item End -->
                 </div>
 
-                <div class="col-lg-3 col-md-6">
-                    <!-- Service Item Start -->
+                <div class="col-md-4 col-12">
                     <div class="service-item">
                         <div class="icon-box">
                             <img src="images/icon-service-8.svg" alt="">
                         </div>
-
                         <div class="service-body">
                             <h3>VALVES</h3>
                         </div>
-
-                        <!-- <div class="service-footer">
-                            <a href="service-single.html" class="service-btn">
-                                <img src="images/arrow-dark.svg" alt="">
-                            </a>
-                        </div> -->
                     </div>
-                    <!-- Service Item End -->
                 </div>
 
-                <div class="col-lg-3 col-md-6">
-                    <!-- Service Item Start -->
+                <div class="col-md-4 col-12">
                     <div class="service-item">
                         <div class="icon-box">
                             <img src="images/icon-service-8.svg" alt="">
                         </div>
-
                         <div class="service-body">
                             <h3>CONSTRUCTION</h3>
                         </div>
-
-                        <!-- <div class="service-footer">
-                            <a href="service-single.html" class="service-btn">
-                                <img src="images/arrow-dark.svg" alt="">
-                            </a>
-                        </div> -->
                     </div>
-                    <!-- Service Item End -->
                 </div>
+
             </div>
         </div>
     </div>
+
     <!-- Page Services End -->
 
     <!-- Footer Start -->
     <?php include 'footer.php'; ?>
     <!-- Footer End -->
 
-    <?php include 'scriptsImport.php' ; ?>
+    <?php include 'scriptsImport.php'; ?>
 </body>
+
 </html>
