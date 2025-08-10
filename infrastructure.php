62af869bf7d834851b191229c6fe5426f7f3cc5f
Nidhi Jain <nidhijain5f2005@gmail.com>
2025-08-10 08:20:33 +0530

    infrastructure update

--- infrastructure.php
+++ infrastructure.php
@@ -1,11 +1,33 @@
+<?php
+$galleryCategories = [
+    "Panchratna Steel" => [
+        "images/panchratnaContent/Infrastructure/Panchratna_Steel/gallery-1.jpg",
+        "images/panchratnaContent/Infrastructure/Panchratna_Steel/gallery-2.jpg",
+        "images/panchratnaContent/Infrastructure/Panchratna_Steel/gallery-3.jpg"
+    ],
+    "Lakshya Steel" => [
+        "images/panchratnaContent/Infrastructure/Lakshya_Steel/gallery-1.jpg",
+        "images/panchratnaContent/Infrastructure/Lakshya_Steel/gallery-2.jpg",
+        "images/panchratnaContent/Infrastructure/Lakshya_Steel/gallery-3.jpg",
+        "images/panchratnaContent/Infrastructure/Lakshya_Steel/gallery-4.jpg"
+    ],
+    "POPL" => [
+        "images/panchratnaContent/Infrastructure/POPL/gallery-1.jpg",
+        "images/panchratnaContent/Infrastructure/POPL/gallery-2.jpg",
+        "images/panchratnaContent/Infrastructure/POPL/gallery-3.jpg"
+    ]
+];
+?>
 <!DOCTYPE html>
 <html lang="zxx">
+
 <head>
     <?php $page_title = "Infrastructure - Panchratna Overseas";
     $page_description = "Learn about Panchratna Overseas";
     include 'headConfig.php';
     ?>
 </head>
+
 <body>
 
     <!-- Preloader Start -->
@@ -18,149 +40,60 @@
 
 
     <!-- Page Header Start -->
-	<div class="page-header page-header-infrastructure parallaxie">
-		<div class="container">
-			<div class="row align-items-center">
-				<div class="col-lg-12">
-					<!-- Page Header Box Start -->
-					<div class="page-header-box">
-						<h1 class="text-anime-style-2" data-cursor="-opaque">Our <span>Infrastructure</span></h1>
-						<nav class="wow fadeInUp">
+    <div class="page-header page-header-infrastructure parallaxie">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-12">
+                    <!-- Page Header Box Start -->
+                    <div class="page-header-box">
+                        <h1 class="text-anime-style-2" data-cursor="-opaque">Our <span>Infrastructure</span></h1>
+                        <nav class="wow fadeInUp">
                             <ol class="breadcrumb">
-								<li class="breadcrumb-item"><a href="./">home</a></li>
-								<li class="breadcrumb-item active" aria-current="page">Infrastructure</li>
-							</ol>
-						</nav>
-					</div>
-					<!-- Page Header Box End -->
-				</div>
-			</div>
-		</div>
-	</div>
-	<!-- Page Header End -->
+                                <li class="breadcrumb-item"><a href="./">home</a></li>
+                                <li class="breadcrumb-item active" aria-current="page">Infrastructure</li>
+                            </ol>
+                        </nav>
+                    </div>
+                    <!-- Page Header Box End -->
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Page Header End -->
 
     <!-- Photo Gallery Section Start -->
-	<div class="page-gallery">
-		<div class="container">
-			<!-- gallery section start -->
-			<div class="row gallery-items page-gallery-box">
-                <div class="col-lg-4 col-6">
-                    <!-- image gallery start -->
-                    <div class="photo-gallery wow fadeInUp" data-cursor-text="View">
-                        <a href="images/gallery-1.jpg">
-                            <figure class="image-anime">
-                                <img src="images/gallery-1.jpg" alt="">
-                            </figure>
-                        </a>
+    <div class="page-gallery">
+        <div class="container">
+            <?php foreach ($galleryCategories as $category => $images): ?>
+                <div class="category-section mb-5">
+                    <h3 class="mb-4"><?php echo htmlspecialchars($category); ?></h3>
+                    <div class="row gallery-items page-gallery-box">
+                        <?php foreach ($images as $index => $image): ?>
+                            <div class="col-lg-4 col-6">
+                                <!-- image gallery start -->
+                                <div class="photo-gallery wow fadeInUp" data-wow-delay="<?php echo 0.2 * $index; ?>s"
+                                    data-cursor-text="View">
+                                    <a href="<?php echo $image; ?>">
+                                        <figure class="image-anime">
+                                            <img src="<?php echo $image; ?>" alt="">
+                                        </figure>
+                                    </a>
+                                </div>
+                                <!-- image gallery end -->
+                            </div>
+                        <?php endforeach; ?>
                     </div>
-                    <!-- image gallery end -->
                 </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
+    <!-- Photo Gallery Section End -->
 
-                <div class="col-lg-4 col-6">
-                    <!-- image gallery start -->
-                    <div class="photo-gallery wow fadeInUp" data-wow-delay="0.2s" data-cursor-text="View">
-                        <a href="images/gallery-2.jpg">
-                            <figure class="image-anime">
-                                <img src="images/gallery-2.jpg" alt="">
-                            </figure>
-                        </a>
-                    </div>
-                    <!-- image gallery end -->
-                </div>
-
-                <div class="col-lg-4 col-6">
-                    <!-- image gallery start -->
-                    <div class="photo-gallery wow fadeInUp" data-wow-delay="0.4s" data-cursor-text="View">
-                        <a href="images/gallery-3.jpg">
-                            <figure class="image-anime">
-                                <img src="images/gallery-3.jpg" alt="">
-                            </figure>
-                        </a>
-                    </div>
-                    <!-- image gallery end -->
-                </div>
-
-                <div class="col-lg-4 col-6">
-                    <!-- image gallery start -->
-                    <div class="photo-gallery wow fadeInUp" data-wow-delay="0.6s" data-cursor-text="View">
-                        <a href="images/gallery-4.jpg">
-                            <figure class="image-anime">
-                                <img src="images/gallery-4.jpg" alt="">
-                            </figure>
-                        </a>
-                    </div>
-                    <!-- image gallery end -->
-                </div>
-
-                <div class="col-lg-4 col-6">
-                    <!-- image gallery start -->
-                    <div class="photo-gallery wow fadeInUp" data-wow-delay="0.8s" data-cursor-text="View">
-                        <a href="images/gallery-5.jpg">
-                            <figure class="image-anime">
-                                <img src="images/gallery-5.jpg" alt="">
-                            </figure>
-                        </a>
-                    </div>
-                    <!-- image gallery end -->
-                </div>
-
-                <div class="col-lg-4 col-6">
-                    <!-- image gallery start -->
-                    <div class="photo-gallery wow fadeInUp" data-wow-delay="1s" data-cursor-text="View">
-                        <a href="images/gallery-6.jpg">
-                            <figure class="image-anime">
-                                <img src="images/gallery-6.jpg" alt="">
-                            </figure>
-                        </a>
-                    </div>
-                    <!-- image gallery end -->
-                </div>
-                
-                <div class="col-lg-4 col-6">
-                    <!-- image gallery start -->
-                    <div class="photo-gallery wow fadeInUp" data-wow-delay="1.2s" data-cursor-text="View">
-                        <a href="images/gallery-7.jpg">
-                            <figure class="image-anime">
-                                <img src="images/gallery-7.jpg" alt="">
-                            </figure>
-                        </a>
-                    </div>
-                    <!-- image gallery end -->
-                </div>
-
-                <div class="col-lg-4 col-6">
-                    <!-- image gallery start -->
-                    <div class="photo-gallery wow fadeInUp" data-wow-delay="1.4s" data-cursor-text="View">
-                        <a href="images/gallery-8.jpg">
-                            <figure class="image-anime">
-                                <img src="images/gallery-8.jpg" alt="">
-                            </figure>
-                        </a>
-                    </div>
-                    <!-- image gallery end -->
-                </div>
-
-                <div class="col-lg-4 col-6">
-                    <!-- image gallery start -->
-                    <div class="photo-gallery wow fadeInUp" data-wow-delay="1.6s" data-cursor-text="View">
-                        <a href="images/gallery-9.jpg">
-                            <figure class="image-anime">
-                                <img src="images/gallery-9.jpg" alt="">
-                            </figure>
-                        </a>
-                    </div>
-                    <!-- image gallery end -->
-                </div>
-			</div>
-			<!-- gallery section end -->
-		</div>
-	</div>
-	<!-- Photo Gallery Section End -->
-
-       <!-- Footer Start -->
+    <!-- Footer Start -->
     <?php include 'footer.php'; ?>
     <!-- Footer End -->
 
-    <?php include 'scriptsImport.php' ; ?>
+    <?php include 'scriptsImport.php'; ?>
 </body>
+
 </html>
